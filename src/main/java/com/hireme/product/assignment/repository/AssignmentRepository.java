bfffdfdbc1fc4b6ed9d1d222971156f831458889
lwseng11 <12lws11@gmail.com>
2023-10-17 12:59:07 +0800

    - payment transaction take in user and assignment id

--- src/main/java/com/hireme/product/assignment/repository/AssignmentRepository.java
+++ src/main/java/com/hireme/product/assignment/repository/AssignmentRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     List<Assignment> findByStatus(String status);
     List<Assignment> findByAssignmentType(AssignmentType type);
+
+    Optional<Assignment> findByAssignmentId(Long id);
 }
--- src/main/java/com/hireme/product/payment/Resource/PaymentResource.java
+++ src/main/java/com/hireme/product/payment/Resource/PaymentResource.java
@@ -2,8 +2,9 @@ package com.hireme.product.payment.Resource;
 
 import com.hireme.product.payment.dto.PaymentCheckout;
 import com.hireme.product.payment.dto.PaymentDto;
-import com.hireme.product.payment.service.Service;
-import com.stripe.exception.StripeException;
+import com.hireme.product.payment.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -14,29 +15,52 @@ import java.util.List;
 @RestController
 @RequestMapping("api/payments")
 public class PaymentResource {
-    private final Service paymentService;
+    private final PaymentService paymentService;
 
     Logger logger = LoggerFactory.getLogger(PaymentResource.class);
 
-    public PaymentResource(Service paymentService) {
+    public PaymentResource(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
+    // used during checkout. The session id returned is for stripe purpose
+    @Operation(
+            summary = "Create a new payment session"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "session created successfully"
+    )
     @PostMapping("/payment")
-    public String createCheckoutSession( @RequestBody PaymentCheckout request){
+    public String createCheckoutSession( @RequestHeader("USER-ID") String userId,@RequestBody PaymentCheckout request){
         logger.info("In createCheckoutSession");
-        return paymentService.checkout(request);
+        return paymentService.checkout(request, userId);
     }
 
+    // once payment is successful, call this endpoint
+    @Operation(
+            summary = "Check payment"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "payment is successful"
+    )
     @PutMapping("save/{sessionId}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void saveSuccessfulResource(@PathVariable String sessionId){
+    public void saveSuccessfulResource(@RequestHeader("USER-ID") String userId, @PathVariable String sessionId){
         logger.info("saving session id " + sessionId);
         paymentService.successPayment(sessionId);
     }
 
+    @Operation(
+            summary = "payemnt history"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "history return"
+    )
     @GetMapping("history/{userId}")
-    public List<PaymentDto> getPastPayments(@PathVariable String userId){
+    public List<PaymentDto> getPastPayments(@RequestHeader("USER-ID") String userHdrId, @PathVariable String userId){
         logger.info("geting historical payment");
         return paymentService.getPastPayment(userId);
     }
